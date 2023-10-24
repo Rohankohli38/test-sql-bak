f22bbbf584ed527b6e1c36b5fb6c0e7e3f7873a3
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2023-10-24 11:56:38 +0530

    Update devBackup.sql

--- devBackup.sql
+++ devBackup.sql
@@ -1,4 +1,4 @@
-BACKUP DATABASE DevSQLShackDemo
-    TO DISK = '/home/rishi/DEV/DevDB.bak'
+BACKUP DATABASE DevDatabase
+    TO DISK = '/home/rishi/database/prodDatabase/devDB.bak'
     WITH FORMAT;
 GO
