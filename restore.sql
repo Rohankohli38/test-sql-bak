632f7d719cdb7cbab4ec8437ef26c9fd9d38ad6a
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2023-10-24 09:46:37 +0530

    Update restore.sql

--- restore.sql
+++ restore.sql
@@ -1,6 +1,6 @@
 -- Restore the backup to a new or existing database
-RESTORE DATABASE YourTargetDatabase
-FROM DISK = 'C:\Backup\YourSourceDatabase.bak'
-WITH MOVE 'YourSourceDatabase' TO 'C:\Data\YourTargetDatabase.mdf',
-     MOVE 'YourSourceDatabase_log' TO 'C:\Data\YourTargetDatabase_log.ldf',
+RESTORE DATABASE TestSQLShackDemo
+FROM DISK = '/home/rishi/DBbackups/db.bak'
+WITH MOVE 'ProdSQLShackDemo' TO '/home/rishi/TestSQLShackDemo/db.bak',
+     MOVE 'ProdSQLShackDemo_log' TO '/home/rishi/TestSQLShackDemo/db.bak_log.bak',
      REPLACE;
